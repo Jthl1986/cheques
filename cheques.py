24137398105f93045dd309a319eca6c781ed47cf
Santacecilia <jorgesantacecilia@gmail.com>
2026-02-06 15:25:03 -0300

    9

--- cheques.py
+++ cheques.py
@@ -195,13 +195,19 @@ def main():
             top_cantidad = top_firmantes.sort_values('cantidad_cheques', ascending=False).head(5)
             top_cantidad['monto_total'] = top_cantidad['monto_total'].apply(lambda x: f'${x:,.0f}')
             top_cantidad['monto_total_ajustado'] = top_cantidad['monto_total_ajustado'].apply(lambda x: f'${x:,.0f}')
-            st.dataframe(top_cantidad[['firmante', 'cantidad_cheques', 'monto_total', 'monto_total_ajustado', 'promedio_diferencia_dias']])
+            st.dataframe(
+                top_cantidad[['firmante', 'cantidad_cheques', 'monto_total', 'monto_total_ajustado', 'promedio_diferencia_dias']], 
+                use_container_width=True
+            )
 
             st.subheader("Top 5 por Monto Ajustado")
             top_monto = top_firmantes.sort_values('monto_total_ajustado', ascending=False).head(5)
             top_monto['monto_total'] = top_monto['monto_total'].apply(lambda x: f'${x:,.0f}')
             top_monto['monto_total_ajustado'] = top_monto['monto_total_ajustado'].apply(lambda x: f'${x:,.0f}')
-            st.dataframe(top_monto[['firmante', 'cantidad_cheques', 'monto_total', 'monto_total_ajustado', 'promedio_diferencia_dias']])
+            st.dataframe(
+                top_monto[['firmante', 'cantidad_cheques', 'monto_total', 'monto_total_ajustado', 'promedio_diferencia_dias']], 
+                use_container_width=True
+            )
 
             # Gráfico de análisis por firmante
             #st.subheader("Análisis Temporal por Firmante")
